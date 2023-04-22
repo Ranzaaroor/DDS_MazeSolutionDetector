5b5e7bf5a9c6f5f2683673e06bd8cd3c1d9ca1d8
Ranzaaroor <77457183+Ranzaaroor@users.noreply.github.com>
2023-04-22 10:05:15 +0300

    Add files via upload

---
+++ Maze.java
@@ -0,0 +1,199 @@
+
+
+
+import java.util.ArrayList; 
+import java.awt.Color; 
+ 
+/** 
+ * A class for decomposing images into connected components. 
+ */ 
+public class Maze { 
+ 
+   private UnionFind uf; 
+   private DisplayImage image; 
+   private int startX;   // x-coordinate for the start pixel
+   private int startY;	 // y-coordinate for the start pixel
+   private int endX;	 // x-coordinate for the end pixel
+   private int endY;	 // y-coordinate for the end pixel
+  
+  /** 
+    * Constructor - reads image, and decomposes it into its connected components.
+    * After calling the constructor, the mazeHasSolution() and areConnected() methods
+	* are expect to return a correct solution.
+    * 
+    * @param fileName name of image file to process. 
+    */ 
+   public Maze (String fileName, Color c) { 
+	    image = new DisplayImage (fileName); 
+	    uf = new UnionFind (image.width() * image.height()); 
+	    setStartingPoints(c);
+	    connectAll(c); 
+   } 
+ 
+
+
+
+
+private void setStartingPoints(Color c) {
+    int xMax = image.width() - 1; 
+    int yMax = image.height() - 1; 
+    boolean isColored = false;
+
+    for (int x = 0; x <= xMax; x++) 
+       for (int y = 0; y <= yMax; y++) {
+    	   if(image.isRed(x,y)) {
+    		   image.set(x,y,c);   	   
+    		   if(!isColored) {
+    			   this.startX=x;
+       	   			this.startY=y;
+       	   			isColored=true;
+    		   }
+    		   else {
+    			   this.endX=x;
+      	   			this.endY=y;
+    		   }
+    	   }   
+    }	
+}
+
+/** 
+    * Generates a unique integer id from (x, y) coordinates. 
+    * 
+    * @param x x-coordinate. 
+    * @param y y-coordinate. 
+    * @return unique id. 
+    */ 
+   private int pixelToId (int x, int y) { 
+         return y * image.width() + x + 1; 
+   } 
+ 
+   /** 
+    * Connects two pixels if they both belong to the same image 
+    * area (background or foreground), and are not already connected. 
+    * It is assumed that the pixels are neighbours.
+    * 
+    * @param x1 x-coordinate of first pixel. 
+    * @param y1 y-coordinate of first pixel. 
+    * @param x2 x-coordinate of second pixel. 
+    * @param y2 y-coordinate of second pixel. 
+    */ 
+ public void connect (int x1, int y1, int x2, int y2) {  		   
+	if (image.isOn (x1, y1) == image.isOn (x2, y2))  { 
+			int pixel1Rep = uf.find (pixelToId (x1, y1)); 
+		    int pixel2Rep = uf.find (pixelToId (x2, y2)); 
+		       if (pixel1Rep != pixel2Rep) 
+		           uf.union(pixel1Rep, pixel2Rep); 
+		      } 
+		    
+   } 
+ 
+
+ /** 
+  * Connects each pixel to all it's neighbors. 
+  */ 
+ private void connectAll(Color c) { 
+
+    int xMax = image.width() - 1; 
+    int yMax = image.height() - 1; 
+
+    for (int x = 0; x <= xMax; x++) 
+       for (int y = 0; y <= yMax; y++) {
+          if (y < yMax) 
+             connect (x, y, x, y + 1); 
+          if (x < xMax) 
+             connect (x, y, x + 1, y); 
+      //    if (x<xMax && y< yMax)
+      //  	  connect(x,y,x+1,y+1);
+       } 
+
+  // Special case - handle lower right pixel. 
+ //  connect (xMax, yMax, xMax, yMax - 1);       
+ //   connect (xMax, yMax, xMax - 1, yMax); 
+ } 
+
+ /** 
+  * Checks if two pixels are connected (belong to the same component). 
+  * 
+  * @param x1 x-coordinate of first pixel. 
+  * @param y1 y-coordinate of first pixel. 
+  * @param x2 x-coordinate of second pixel. 
+  * @param y2 y-coordinate of second pixel. 
+  * @return true if the pixels are connected, false otherwise. 
+  */ 
+ public boolean areConnected (int x1, int y1, int x2, int y2) { 
+    return uf.find(pixelToId (x1, y1)) == uf.find (pixelToId (x2, y2)); 
+ } 
+
+ /** 
+  * Finds the number of components in the image. 
+  * 
+  * @return the number of components in the image 
+  */ 
+ public int getNumComponents() { 
+    return uf.getNumSets(); 
+ } 
+
+ 
+   /**
+    * Returns true if and only if the maze has a solution.
+    * In this case, the start and end pixel will belong to the same connected component.
+    * 
+    * @return true if and only if the maze has a solution
+    */
+   public boolean mazeHasSolution(){
+		return areConnected(startX, startY, endX, endY);
+	   //return areConneted(startX,startY,endX,endY);
+   }
+   /** 
+    * Creates a visual representation of the connected components. 
+    * 
+    * @return a new image with each component colored in a random color. 
+    */ 
+   public DisplayImage getComponentImage() { 
+ 
+      DisplayImage compImg = new DisplayImage (image.width(), image.height()); 
+      ArrayList<Integer> usedIds = new ArrayList<Integer>(); 
+      int numComponents = getNumComponents(); 
+      final int MAX_COLOR = 0xffffff; 
+      Color colors[] = new Color[numComponents]; 
+ 
+      colors[0] = new Color (0, 0, 100); 
+      for (int c = 1; c < numComponents; c++) 
+         colors[c] = new Color ((int)(Math.random() * MAX_COLOR)); 
+ 
+      for (int x = 0; x < image.width(); x++) 
+         for (int y = 0; y < image.height(); y++) { 
+            int componentId = uf.find (pixelToId (x, y)); 
+            // Check if this id already exists (inefficient for a large number of components). 
+            int index = -1; 
+            for (int i = 0; i < usedIds.size(); i++) 
+               if (usedIds.get (i) == componentId) 
+                  index = i; 
+            if (index == -1) { 
+               usedIds.add (componentId); 
+               index = usedIds.size() - 1; 
+            } 
+            compImg.set (x, y, colors[index]); 
+         } 
+ 
+      return compImg; 
+   } 
+ 
+   /** 
+    * Various tests for the segmentation functionality. 
+    * 
+    * @param args command line arguments - name of file to process. 
+    */ 
+   public static void main (String[] args) { 
+ 
+	   Maze maze = new Maze (args[0], Color.black); 
+	      System.out.println (maze.mazeHasSolution());	   
+ 
+      System.out.println (maze.mazeHasSolution());
+ 
+      System.out.println ("Number of components: " + maze.getNumComponents()); 
+ 
+      DisplayImage compImg = maze.getComponentImage(); 
+      compImg.show(); 
+   } 
+} 
---
+++ UnionFind.java
@@ -0,0 +1,171 @@
+
+/** 
+ * Implementation of the Union-Find ADT. 
+ */ 
+public class UnionFind { 
+ 
+   private static final int ROOT = 0; 
+   private int up[]; 
+   private int weight[]; 
+   int numSets; 
+ 
+ 
+   /** 
+    * Constructor - initializes up and weight arrays. 
+    * 
+    * @param numElements initial number of singleton sets. 
+    */ 
+   public UnionFind (int numElements) { 
+ 
+      up = new int[numElements + 1]; 
+      for (int i = 1; i < up.length; i++) 
+         up[i] = ROOT; 
+ 
+      weight = new int[numElements + 1]; 
+      for (int i = 1; i < weight.length; i++) 
+         weight[i] = 1; 
+ 
+      numSets = numElements; 
+   } 
+ 
+   /** 
+    * Unites two sets using weigthed union. 
+	* Assumes that the given elements are representatives of their sets.
+    * 
+    * @param i representative of first set. 
+    * @param j representative of second set. 
+    */ 
+   public void union (int i, int j) { 
+ 
+      if (up[i] != 0 || up[j] != 0) 
+         throw new IllegalArgumentException ("i and j must be set representatives"); 
+       
+      if (i == j) 
+         return; 
+ 
+      numSets--; 
+ 
+      int newWeight = weight[i] + weight[j]; 
+      if (weight[i] < weight[j]) { 
+         up[i] = j; 
+         weight[j] = newWeight; 
+      } 
+      else { 
+         up[j] = i; 
+         weight[i] = newWeight; 
+      } 
+   } 
+ 
+   /** 
+    * Finds the set representative, and applies path compression. 
+    * 
+    * @param i the element to search. 
+    * @return the representative of the group that contains i. 
+    */ 
+   public int find (int i) { 
+ 
+      int r = i; 
+ 
+      while (up[r] != ROOT) 
+         r = up[r]; 
+ 
+      // Compress path. 
+      if (i != r) { 
+         int k = up[i]; 
+         while (k != r) { 
+            up[i] = r; 
+            i = k; 
+            k = up[k]; 
+         } 
+      } 
+ 
+      return r; 
+   } 
+ 
+   /** 
+    * Find the current number of sets. 
+    * 
+    * @return the number of set. 
+    */ 
+   public int getNumSets() { 
+      return numSets; 
+   } 
+ 
+   /** 
+    * Prints the contents of the up array. 
+    */ 
+   public void debugPrintUp() { 
+ 
+      System.out.print ("up:     "); 
+      for (int i = 1; i < up.length; i++) 
+         System.out.print (up[i] + " "); 
+      System.out.println (""); 
+   } 
+ 
+   /** 
+    * Prints the results of running find on all elements. 
+    */ 
+   public void debugPrintFind() { 
+ 
+      System.out.print ("find:   "); 
+      for (int i = 1; i < up.length; i++) 
+         System.out.print (find (i) + " "); 
+      System.out.println (""); 
+   } 
+ 
+   /** 
+    * Prints the contents of the weight array. 
+    */ 
+   public void debugPrintWeight() { 
+ 
+      System.out.print ("weight: "); 
+      for (int i = 1; i < weight.length; i++) 
+         System.out.print (weight[i] + " "); 
+      System.out.println (""); 
+   } 
+ 
+   /** 
+    * Various tests for the Union-Find functionality. 
+    * 
+    * @param args command line arguments - not used. 
+    */ 
+   public static void main (String[] args) { 
+ 
+      UnionFind uf = new UnionFind (10); 
+ 
+      uf.debugPrintUp(); 
+      uf.debugPrintFind(); 
+      uf.debugPrintWeight(); 
+      System.out.println ("Number of sets: " + uf.getNumSets()); 
+      System.out.println (""); 
+ 
+      uf.union (2, 1); 
+      uf.union (3, 2); 
+      uf.union (4, 2); 
+      uf.union (5, 2); 
+ 
+      uf.debugPrintUp(); 
+      uf.debugPrintFind(); 
+      uf.debugPrintWeight(); 
+      System.out.println ("Number of sets: " + uf.getNumSets()); 
+      System.out.println(); 
+ 
+      uf.union (6, 7); 
+      uf.union (8, 9); 
+      uf.union (6, 8); 
+ 
+      uf.debugPrintUp(); 
+      uf.debugPrintFind(); 
+      uf.debugPrintWeight(); 
+      System.out.println ("Number of sets: " + uf.getNumSets()); 
+      System.out.println(); 
+ 
+      uf.find (8); 
+ 
+      uf.debugPrintUp(); 
+      uf.debugPrintFind(); 
+      uf.debugPrintWeight(); 
+      System.out.println ("Number of sets: " + uf.getNumSets()); 
+      System.out.println(); 
+   } 
+} 
